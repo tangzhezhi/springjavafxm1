d5aecac17ab0160e15bb213be24ee2126f51e50b
tang <330882908@qq.com>
2018-10-30 21:42:49 +0800

    练习

--- src/main/java/org/tang/springjavafxm1/M1CardOperateView.java
+++
@@ -1,9 +0,0 @@
-package org.tang.springjavafxm1;
-
-import de.felixroske.jfxsupport.AbstractFxmlView;
-import de.felixroske.jfxsupport.FXMLView;
-
-@FXMLView
-public class M1CardOperateView extends AbstractFxmlView {
-
-}
