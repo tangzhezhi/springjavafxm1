ac342797aadcc131a22ada767dfac0d6dbccee73
330882908@qq.com <330882908@qq.com>
2018-11-25 22:10:26 +0800

    图片处理

--- src/main/java/org/tang/springjavafxm1/controller/picture/FaceController.java
+++ src/main/java/org/tang/springjavafxm1/controller/picture/FaceController.java
@@ -5,33 +5,24 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.Label;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
-import org.apache.log4j.Logger;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Rect;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.objdetect.CascadeClassifier;
-import org.opencv.videoio.VideoCapture;
-import org.springframework.context.annotation.Lazy;
-import org.tang.springjavafxm1.utils.OpenCvUtils;
+import javafx.scene.control.TextArea;
 
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.ResourceBundle;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
 
-@Lazy
 @FXMLController
 public class FaceController implements Initializable {
 
@@ -48,9 +39,25 @@ public class FaceController implements Initializable {
 	private Button selectFileDirBtn;
 
 	@FXML
+	private Button saveFileDirBtn;
+
+	@FXML
 	private Button selectFileBtn;
 
 	private File[] waitDealFile ;
+
+	private static File saveFileDir ;
+
+	@FXML
+	private TextArea resultArea;
+
+	@FXML
+	private Label selectFileDirLabel;
+
+	@FXML
+	private Label saveFileDirLabel;
+
+
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -62,6 +69,7 @@ public class FaceController implements Initializable {
 		if (file != null && file.isDirectory()) {
 			System.out.println(file.getName());
 			waitDealFile = file.listFiles();
+			selectFileDirLabel.setText(file.getAbsolutePath());
 		}
 	}
 
@@ -69,23 +77,40 @@ public class FaceController implements Initializable {
 		System.out.println("deal picture");
 
 		if(waitDealFile!=null && waitDealFile.length > 0){
-			CascadeClassifier faceDetector = new CascadeClassifier("D:\\work\\springjavafxm1\\target\\classes\\config\\lbpcascade_frontalface.xml");
+			//			CascadeClassifier faceDetector = new CascadeClassifier("D:\\work\\springjavafxm1\\target\\classes\\config\\lbpcascade_frontalface.xml");
+			CascadeClassifier faceDetector = null;
+			faceDetector = new CascadeClassifier(	new File(this.getClass().getResource("/").getPath()).getAbsolutePath()+"\\config\\lbpcascade_frontalface.xml");
+			CascadeClassifier finalFaceDetector = faceDetector;
 			Arrays.stream(waitDealFile).forEach(f->{
 				Mat img = Imgcodecs.imread(f.getAbsolutePath());
 				Mat tmp = new Mat();
 				img.copyTo(tmp);
 				MatOfRect faceDetections = new MatOfRect();
-				faceDetector.detectMultiScale(tmp, faceDetections);
-				for(Rect rect : faceDetections.toList()){
-					Mat imgROI = new Mat(tmp, new Rect(rect.x, rect.y, rect.width, rect.height));
-					Imgcodecs.imwrite("C:\\Users\\Administrator\\Desktop\\11\\test.jpg",imgROI);
+				finalFaceDetector.detectMultiScale(tmp, faceDetections);
 
-				}
+				resultArea.setPrefRowCount(faceDetections.toList().size());
+				resultArea.setPrefRowCount(2);
+
+				faceDetections.toList().parallelStream().forEach(rect->{
+					Mat imgROI = new Mat(tmp, new Rect(rect.x, rect.y, rect.width, rect.height));
+					String tmpStr = saveFileDir.getAbsolutePath()+File.separator+f.getName();
+					Imgcodecs.imwrite(tmpStr,imgROI);
+					resultArea.appendText(tmpStr+"\n");
+				});
+
+
+//				for(Rect rect : faceDetections.toList()){
+//					Mat imgROI = new Mat(tmp, new Rect(rect.x, rect.y, rect.width, rect.height));
+////					Imgcodecs.imwrite("C:\\Users\\Administrator\\Desktop\\11\\test.jpg",imgROI);
+//					System.out.println("f.getName:::::::"+f.getName());
+//
+//					String tmpStr = saveFileDir.getAbsolutePath()+File.separator+f.getName();
+//
+//					Imgcodecs.imwrite(tmpStr,imgROI);
+//					resultArea.appendText(tmpStr+"\n");
+//				}
 			});
 		}
-
-
-
 	}
 
 	private static void configureFileChooser(final FileChooser fileChooser){
@@ -109,6 +134,18 @@ public class FaceController implements Initializable {
 		File file = fileChooser.showOpenDialog(selectFileDirBtn.getContextMenu());
 		if (file != null) {
 			openFile(file);
+		}
+	}
+
+	/**
+	 * 保存到
+	 * @param actionEvent
+	 */
+	public void saveFileDirAction(ActionEvent actionEvent) {
+		File file = directoryChooser.showDialog(saveFileDirBtn.getContextMenu());
+		if (file != null && file.isDirectory()) {
+			saveFileDir = file;
+			saveFileDirLabel.setText(file.getAbsolutePath());
 		}
 	}
 }
